44f6af8233693a9ec7241fd07a9f8b10be1711df
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-20 14:48:08 +0000

    rename arposters module to prep

--- prep/__main__.py
+++ prep/__main__.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import sys
 import time
 import numpy as np
-from arposters.camera_calibration import CameraCalibration
+from prep.camera_calibration import CameraCalibration
 
 calib = CameraCalibration()
 calib.start()
