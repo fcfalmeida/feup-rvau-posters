8ac6d808fd2ad4da27193558d5e63a4cdc81f66f
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-21 14:40:33 +0000

    implement Add Image to Database

--- prep/database.py
+++ prep/database.py
@@ -1,4 +1,26 @@
+import cv2 as cv
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
 class Database:
+
+    DB_DIR = "prep/db/"
+    SAVE_EXTENSION = ".png"
     
     def add_image(self):
-        print("import")
+        root = Tk()
+        root.withdraw()
+        filename = askopenfilename(filetypes=[("Image Files", ".png .jpg .jpeg")])
+        root.destroy()
+
+        img_name = filename.split("/")[-1].split(".")[-2]
+
+        up_img = cv.imread(filename)
+        right_img = cv.rotate(up_img, cv.ROTATE_90_CLOCKWISE)
+        down_img = cv.rotate(up_img, cv.ROTATE_180)
+        left_img = cv.rotate(up_img, cv.ROTATE_90_COUNTERCLOCKWISE)
+
+        cv.imwrite(self.DB_DIR + img_name + "_up" + self.SAVE_EXTENSION, up_img)
+        cv.imwrite(self.DB_DIR + img_name + "_right" + self.SAVE_EXTENSION, right_img)
+        cv.imwrite(self.DB_DIR + img_name + "_down" + self.SAVE_EXTENSION, down_img)
+        cv.imwrite(self.DB_DIR + img_name + "_left" + self.SAVE_EXTENSION, left_img)
