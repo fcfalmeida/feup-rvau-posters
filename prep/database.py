a31ef3e85a7570e32d3508a41a8a463bda75a02d
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-27 14:27:35 +0000

    save film poster features

--- prep/database.py
+++ prep/database.py
@@ -7,6 +7,7 @@ from tkinter.filedialog import askopenfilename
 from prep.menu import FunctionItem
 from prep.film import Film
 
+
 class Database:
 
     DB_DIR = "prep/db/"
@@ -16,11 +17,12 @@ class Database:
     def __init__(self):
         self.data = []
         self._load()
-    
+
     def add_image(self):
         root = Tk()
         root.withdraw()
-        filename = askopenfilename(filetypes=[("Image Files", ".png .jpg .jpeg")])
+        filename = askopenfilename(
+            filetypes=[("Image Files", ".png .jpg .jpeg")])
         root.destroy()
 
         img_name = filename.split("/")[-1].split(".")[-2]
@@ -32,7 +34,9 @@ class Database:
 
         cv.imwrite(self.DB_DIR + film_name + self.SAVE_EXTENSION, img)
 
-        film = Film(film_name, film_score, None, None)
+        keypoints, descriptors = self._extract_features(img)
+
+        film = Film(film_name, film_score, keypoints, descriptors)
         self.data.append(film)
 
         self._save()
@@ -55,6 +59,17 @@ class Database:
 
         self._remove_film(film_name)
 
+    def get_films_with_images(self):
+        films_with_images = []
+
+        for film in self.data:
+            img_path = glob.glob(f"{self.DB_DIR}{film.title}.*")[0]
+            img = cv.imread(img_path)
+
+            films_with_images.append((film, img))
+
+        return films_with_images
+
     def _remove_film(self, title):
         for film in self.data:
             if film.title == title:
@@ -65,9 +80,45 @@ class Database:
     def _load(self):
         try:
             data = pickle.load(open(self.DB_DIR + self.PERSISTENCE_FILE, "rb"))
+
+            for film in data:
+                film.keypoints = self._deserialize_keypoints(film.keypoints)
+
             self.data = data
         except FileNotFoundError:
             pass
 
     def _save(self):
-        pickle.dump(self.data, open(self.DB_DIR + self.PERSISTENCE_FILE, "wb"))
+        data = self.data
+
+        for film in data:
+            film.keypoints = self._serialize_keypoints(film.keypoints)
+
+        pickle.dump(data, open(self.DB_DIR + self.PERSISTENCE_FILE, "wb"))
+
+    def _extract_features(self, img):
+        minHessian = 400
+        detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
+        keypoints, descriptors = detector.detectAndCompute(
+            cv.cvtColor(img, cv.COLOR_BGR2GRAY), None)
+
+        return keypoints, descriptors
+
+    def _serialize_keypoints(self, keypoints):
+        ser_keypoints = []
+
+        for keypoint in keypoints:
+            ser_keypoints.append((keypoint.pt, keypoint.size, keypoint.angle, keypoint.response, keypoint.octave,
+                                  keypoint.class_id))
+
+        return ser_keypoints
+
+    def _deserialize_keypoints(self, ser_keypoints):
+        keypoints = []
+
+        for keypoint in ser_keypoints:
+            keypoints.append(
+                cv.KeyPoint(x=keypoint[0][0], y=keypoint[0][1], _size=keypoint[1], _angle=keypoint[2],
+                            _response=keypoint[3], _octave=keypoint[4], _class_id=keypoint[5]))
+
+        return keypoints
