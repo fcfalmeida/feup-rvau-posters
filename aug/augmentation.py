bc2a9f4e7cf1294ca4312e4eb6242ce6afc97ad6
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-11-30 00:16:52 +0000

    draw cubes depending on score

--- aug/augmentation.py
+++ aug/augmentation.py
@@ -12,6 +12,7 @@ class Augmentation:
     MIN_GOOD_MATCHES = 120
     HESSIAN_THRESHOLD = 400
     CUBE_SIZE = 200
+    CUBE_Z_OFFSET = 50
 
     def __init__(self, cbrows=9, cbcols=6):
         self.root = None
@@ -48,8 +49,13 @@ class Augmentation:
                 # -- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
                 keypoints_obj = film.keypoints
                 descriptors_obj = film.descriptors
+
                 keypoints_scene, descriptors_scene = detector.detectAndCompute(
                     gray_frame, None)
+
+                # Check if any descriptors can be found in the scene
+                if descriptors_scene is None:
+                    continue
 
                 # -- Step 2: Matching descriptor vectors with a FLANN based matcher
                 # Since SURF is a floating-point descriptor NORM_L2 is used
@@ -101,7 +107,8 @@ class Augmentation:
                     cv.putText(
                         frame, film.title, top_left_corner, cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1, 1)
 
-                    frame = self._display_score(obj_corners, scene_corners, frame)
+                    frame = self._display_score(
+                        obj_corners, scene_corners, frame, film.score)
 
             cv.imshow('Augmentation', frame)
 
@@ -118,30 +125,48 @@ class Augmentation:
         # Prevents freezing when closing the window for some reason
         cv.waitKey(1)
 
-    def _display_score(self, obj_corners, scene_corners, frame):
+    def _display_score(self, obj_corners, scene_corners, frame, score):
+        poster_width = obj_corners[1, 0, 0] - obj_corners[0, 0, 0]
+        poster_height = obj_corners[3, 0, 1] - obj_corners[0, 0, 1]
+
         size = Augmentation.CUBE_SIZE
+
+        x_offset = poster_width / 2 - size / 2
+        y_offset = poster_height / 2 - size / 2
+
         # Find the rotation and translation vectors.
         ret, rvecs, tvecs = cv.solvePnP(
             self._to_3d_points(obj_corners), scene_corners, self.camera_params.mtx, self.camera_params.dist)
 
-        cube = np.float32([[0,0,0], [0,size,0], [size,size,0], [size,0,0],
-                        [0,0,-size],[0,size,-size],[size,size,-size],[size,0,-size] ])
-        # project 3D points to image plane
-        imgpts, jac = cv.projectPoints(cube, rvecs, tvecs, self.camera_params.mtx, self.camera_params.dist)
+        return self._draw_cubes(frame, x_offset, y_offset, score, rvecs, tvecs)
 
-        return self._draw_cube(frame, scene_corners, imgpts)
-
-
-    def _draw_cube(self, frame, scene_corners, imgpts):
-        imgpts = np.int32(imgpts).reshape(-1,2)
+    def _draw_cube(self, frame, cubepts):
+        cubepts = np.int32(cubepts).reshape(-1, 2)
         # draw ground floor in green
-        img = cv.drawContours(frame, [imgpts[:4]],-1,(255,255,255),2)
+        img = cv.drawContours(frame, [cubepts[:4]], -1, (255, 255, 255), 2)
         # draw pillars in blue color
-        for i,j in zip(range(4),range(4,8)):
-            img = cv.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255,255,255),2)
+        for i, j in zip(range(4), range(4, 8)):
+            img = cv.line(img, tuple(cubepts[i]), tuple(
+                cubepts[j]), (255, 255, 255), 2)
         # draw top layer in red color
-        img = cv.drawContours(img, [imgpts[4:]],-1,(255,255,255),2)
+        img = cv.drawContours(img, [cubepts[4:]], -1, (255, 255, 255), 2)
         return img
+
+    def _draw_cubes(self, frame, x_offset, y_offset, score, rvecs, tvecs):
+        size = Augmentation.CUBE_SIZE
+
+        for i in range(score):
+            z_offset = -(size + Augmentation.CUBE_Z_OFFSET) * i
+            cube = np.float32([[x_offset, y_offset, z_offset], [x_offset, y_offset+size, z_offset], [x_offset+size, y_offset+size, z_offset], [x_offset+size, y_offset, z_offset],
+                               [x_offset, y_offset, -size+z_offset], [x_offset, y_offset+size, -size+z_offset], [x_offset+size, y_offset+size, -size+z_offset], [x_offset+size, y_offset, -size+z_offset]])
+
+            # project 3D points of the cube to image plane
+            cubepts, jac = cv.projectPoints(
+                cube, rvecs, tvecs, self.camera_params.mtx, self.camera_params.dist)
+
+            frame = self._draw_cube(frame, cubepts)
+
+        return frame
 
     def _to_3d_points(self, points2d):
         points3d = np.empty((4, 1, 3), dtype=np.float32)
